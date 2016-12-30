b75517e869a669beff566215e32414bdf6232074
xuanhoa88 <xuan.0211@gmail.com>
2016-12-31 03:15:03 +0700

    Update ModelTrait.php

--- src/ModelTrait.php
+++ src/ModelTrait.php
@@ -2,16 +2,12 @@
 
 namespace Llama\Database\Eloquent;
 
-use Illuminate\Database\Query\Builder as QueryBuilder;
-use Llama\Database\Eloquent\Relations\BelongsToMany;
-use Llama\Database\Eloquent\Relations\HasManyThrough;
-
 trait ModelTrait
 {
     /**
      * Create a new Eloquent query builder for the model.
      *
-     * @param QueryBuilder $query
+     * @param \Illuminate\Database\Query\Builder $query
      *
      * @return EloquentBuilder|static
      */
