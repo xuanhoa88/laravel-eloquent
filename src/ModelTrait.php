95292fbd1ebb91479268eb1af0754800ca97f34a
xuanhoa88 <xuan.0211@gmail.com>
2016-12-31 03:16:39 +0700

    Update ModelTrait.php

--- src/ModelTrait.php
+++ src/ModelTrait.php
@@ -7,8 +7,7 @@ trait ModelTrait
     /**
      * Create a new Eloquent query builder for the model.
      *
-     * @param \Illuminate\Database\Query\Builder $query
-     *
+     * @param  \Illuminate\Database\Query\Builder  $query
      * @return EloquentBuilder|static
      */
     public function newEloquentBuilder($query)
